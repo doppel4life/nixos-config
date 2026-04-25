005a47a977c293aa841bda7d41ddb61081587ff8
doppel4life <doppel.pers@protonmail.com>
2026-04-25 15:13:29 +0200

    home manager

--- user_conf/doppel-vm/home.nix
+++ user_conf/doppel-vm/home.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}:{
     home.username= "doppel-vm";
-    home.homeDirectory = "/home/doppel-vm/";
+    home.homeDirectory = "/home/doppel-vm";
     home.stateVersion = "26.05";
     home.file.".config/fish".source = ./fish;
 }
