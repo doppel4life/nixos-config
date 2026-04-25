d9e1c36ab039719831392d662284d8a3940247fd
doppel4life <doppel.pers@protonmail.com>
2026-04-25 15:20:08 +0200

    home manager clean up

--- user_conf/doppel-vm/home.nix
+++ user_conf/doppel-vm/home.nix
@@ -1,6 +1,9 @@
-{pkgs, ...}:{
+{...}:{
     home.username= "doppel-vm";
     home.homeDirectory = "/home/doppel-vm";
     home.stateVersion = "26.05";
-    home.file.".config/fish".source = ./fish;
+    home.file.".config/fish" = {
+        source = ./fish;
+        recursive = true;
+    };
 }
