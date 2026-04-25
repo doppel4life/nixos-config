70b948a5521b88d258ffea1155b975c963e17859
doppel4life <doppel.pers@protonmail.com>
2026-04-25 15:00:57 +0200

    home manager

--- flake.nix
+++ flake.nix
@@ -10,13 +10,22 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+
+  outputs = { self, nixpkgs, home-manager,... }@inputs: {
 
     nixosConfigurations.vm = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };
         modules = [
         ./test-vm.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.tony = import ./user_conf/doppel-vm/home.nix;
+            backupFileExtension = "backup";
+          };
       ];
     };
 
