ef0c9a0ed0835651f78220b98133c128e23c7b26
doppel4life <doppel.pers@protonmail.com>
2026-04-30 22:52:00 +0200

    font and style

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    stylix.url = "github:nix-community/stylix";
     home-manager = {
             url = "github:nix-community/home-manager";
             inputs.nixpkgs.follows = "nixpkgs";
@@ -12,13 +13,14 @@
   };
 
 
-  outputs = { self, nixpkgs, home-manager, nvf,... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, nvf, stylix,... }@inputs: {
 
     nixosConfigurations.vm = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };
         modules = [
         ./test-vm.nix
+        stylix.nixosModules.stylix
         home-manager.nixosModules.home-manager
      ];
     };
