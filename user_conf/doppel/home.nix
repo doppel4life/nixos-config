60838a7326e67bceda5e35721dfc0e56847a16ce
doppel4life <doppel.pers@protonmail.com>
2026-05-08 08:35:18 +0200

    added git config

--- user_conf/doppel/home.nix
+++ user_conf/doppel/home.nix
@@ -3,16 +3,14 @@
     home.homeDirectory = "/home/doppel";
     home.stateVersion = "26.05";
 
-    home.file.".config/fish/config.fish".source = ./fish/config.fish;
-
-    home.file.".config/foot/foot.ini".source = ./foot/foot.ini;
-
     #home.file.".config/nvim" = {
     #source = ./nvim;
     #recursive = true;
     #};
 
+    home.file.".config/fish/config.fish".source = ./fish/config.fish;
+    home.file.".config/foot/foot.ini".source = ./foot/foot.ini;
+    home.file.".config/git/config".source = ./git/config;
     home.file.".tmux.conf".source = ./tmux_config/tmux.conf;
-
     home.file.".config/sway/config".source = ./sway/config;
 }
