09ff433e286ed575ce15baedcc0995a1f0042ad1
doppel4life <doppel.pers@protonmail.com>
2026-05-14 21:02:06 +0000

    tmux

--- user_conf/doppel/home.nix
+++ user_conf/doppel/home.nix
@@ -15,7 +15,7 @@
     home.file.".config/fish/config.fish".source = ./fish/config.fish;
     home.file.".config/foot/foot.ini".source = ./foot/foot.ini;
     home.file.".config/git/config".source = ./git/config;
-    home.file.".tmux.conf".source = ./tmux_config/tmux.conf;
+    home.file.".config/tmux/tmux.conf".source = ./tmux_config/tmux.conf;
     home.file.".config/sway/config".source = ./sway/config;
     home.file.".config/wofi" = {
         source = ./wofi;
