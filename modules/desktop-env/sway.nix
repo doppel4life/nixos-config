90035313c4f20fbc74f78741cc689c2f49bfb1b6
doppel4life <doppel.pers@protonmail.com>
2026-05-15 20:53:00 +0000

    roll back

--- modules/desktop-env/sway.nix
+++ modules/desktop-env/sway.nix
@@ -1,13 +1,5 @@
 {pkgs, ...} : {
     
-    services.getty.autologinUser = "doppel";
-
-    programs.fish.loginShellInit = ''
-        if test -z "$DISPLAY" -a (tty) = "/dev/tty1"
-            exec sway
-        fi
-    '';
-
     programs.sway = {
         enable = true;
         wrapperFeatures.gtk = true;
--- user_conf/doppel/fish/config.fish
+++ user_conf/doppel/fish/config.fish
@@ -7,22 +7,17 @@ function fish_prompt
 	printf " in "
 	set_color cyan
 	printf "%s" (prompt_pwd)
-    set_color yellow
-    printf "%s" (fish_git_prompt)
 	set_color normal
 	printf " > "
 end
 
+# Start ssh-agent if not running
+if not set -q SSH_AUTH_SOCK
+    eval (ssh-agent -c)
+    ssh-add ~/.ssh/id_ed25519_work > /dev/null 2>&1
+    ssh-add ~/.ssh/id_ed25519_prof > /dev/null 2>&1
+end
+
 if status is-interactive
-    alias nfu='doas nix flake update'   
-    alias nrs='doas nixos-rebuild switch --impure --flake .#doppelnixer'
-    alias nda='doas nixos-rebuild dry-activate --impure --flake .#doppelnixer'
-    # Git Shortcuts
-    alias gs='git status'
-    alias ga='git add'
-    alias gc='git commit -m'
-    alias gp='git push -u origin'
-    alias gl='git log --oneline --graph --decorate'
-    
-    alias grep='grep --color=auto'
+    # Commands to run in interactive sessions can go here
 end
