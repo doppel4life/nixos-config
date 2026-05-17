b096d77b01940bb316b43971e8e4ef777f46a3a8
doppel4life <doppel.pers@protonmail.com>
2026-05-17 11:04:53 +0000

    gretted

--- modules/desktop-env/sway.nix
+++ modules/desktop-env/sway.nix
@@ -1,5 +1,24 @@
 {pkgs, ...} : {
 
+    services.greetd = {
+        enable = true;
+        settings = {
+            default_sessions = {
+                command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --asterisks --cmd sway";
+            };
+        };
+    };
+
+    systemd.services.greetd.serviceConfig = {
+        Type = "idle";
+        StandardInput = "tty";
+        StandardOutput = "tty";
+        StandardError = "journal";
+        TTYReset = true;
+        TTYVHangup = true;
+        TTYVDisallocate =true;
+    };
+
     programs.sway = {
         enable = true;
         wrapperFeatures.gtk = true;
@@ -9,6 +28,7 @@
             wofi
             brightnessctl
             pulseaudio
+            thunar
             ];
 
     };
